6d92af5c337c9e41c9cf7ae47433ebcac6b9527b
Elena Ivanova <elena.ivanovada@gmail.com>
2023-08-10 17:26:52 +0300

    feat: add media for small size

--- Library/index.js
+++ Library/index.js
@@ -1,2 +1,32 @@
-console.log("100")
+/*(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header-active');
+        } else {
+            header.classList.remove('header-active');  
+        }
+    };
+}());*/
+
+(function () {
+    const burgerItem = document.querySelector('.hamburger');
+    const menu = document.querySelector('.nav');
+    const menuCloseItem = document.querySelector('.header-nav-close');
+    const menuLinks = document.querySelectorAll('.nav-link');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('nav-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('nav-active');
+    });
+    if (window.innerWidth <= 1024) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('nav-active');
+            });
+        }
+    }
+}());
+
 
