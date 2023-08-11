4246c1752b7d1decb45fb3bd0d0ad490febe8914
Elena Ivanova <elena.ivanovada@gmail.com>
2023-08-11 22:23:19 +0300

    feat: add burger menu

--- Library/index.js
+++ Library/index.js
@@ -1,32 +1,28 @@
-/*(function () {
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if (window.pageYOffset > 50) {
-            header.classList.add('header-active');
-        } else {
-            header.classList.remove('header-active');  
-        }
-    };
-}());*/
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector(".header").classList.toggle("open")
+    })
+})
 
-(function () {
-    const burgerItem = document.querySelector('.hamburger');
-    const menu = document.querySelector('.nav');
-    const menuCloseItem = document.querySelector('.header-nav-close');
-    const menuLinks = document.querySelectorAll('.nav-link');
-    burgerItem.addEventListener('click', () => {
-        menu.classList.add('nav-active');
-    });
-    menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('nav-active');
-    });
-    if (window.innerWidth <= 1024) {
-        for (let i = 0; i < menuLinks.length; i += 1) {
-            menuLinks[i].addEventListener('click', () => {
-                menu.classList.remove('nav-active');
-            });
-        }
+
+// Закрыть меню при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        // Действие при клике
+        document.querySelector(".header").classList.remove("open")
     }
-}());
+});
 
+// Закрыть меню при клике вне его
+document.getElementById("menu").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.body.addEventListener('click', event => {
+    if (event._isClickWithInMenu) return;
+    // Действие при клике
+    document.querySelector(".header").classList.remove("open")
+});
 
