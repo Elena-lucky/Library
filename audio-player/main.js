f6cfee4900d4e54f7203a09559463c0439f3fc4f
Elena Ivanova <elena.ivanovada@gmail.com>
2023-09-22 01:17:54 +0300

    feat: add js file

--- audio-player/main.js
+++ audio-player/main.js
@@ -3,7 +3,7 @@ const player = document.querySelector('.player'),
       prevBtn = document.querySelector('.prev'),
       nextBtn = document.querySelector('.next'),
       audio = document.querySelector('.audio'),
-      progress__container = document.querySelector('.progress__container'),
+      progressContainer = document.querySelector('.progress__container'),
       progress = document.querySelector('.progress'),
       title = document.querySelector('.song'),
       cover = document.querySelector('.cover__img'),
@@ -41,3 +41,85 @@ playBtn.addEventListener('click', () => {
     playSong()
     }
 })
+
+function nextSong() {
+    songIndex++
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+} 
+nextBtn.addEventListener('click', nextSong)
+
+function prevSong() {
+    songIndex--
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+} 
+prevBtn.addEventListener('click', prevSong)
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+audio.addEventListener('timeupdate', updateProgress)
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
+
+// timeset
+
+audio.addEventListener("loadeddata", () => {
+      player.querySelector(".time .length").textContent = getTimeCodeFromNum(
+        audio.duration
+      );
+    },
+    false
+  );
+  
+ 
+  const timeline = player.querySelector(".progress__container");
+  timeline.addEventListener("click", e => {
+    const timelineWidth = window.getComputedStyle(timeline).width;
+    const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+    audio.currentTime = timeToSeek;
+  }, false);
+
+  
+setInterval(() => {
+    const progressBar = player.querySelector(".progress");
+    progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+    player.querySelector(".time .current").textContent = getTimeCodeFromNum(
+      audio.currentTime
+    );
+  }, 500);
+
+  
+function getTimeCodeFromNum(num) {
+    let seconds = parseInt(num);
+    let minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+    const hours = parseInt(minutes / 60);
+    minutes -= hours * 60;
+  
+    if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+    return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+      seconds % 60
+    ).padStart(2, 0)}`;
+  }
+  
